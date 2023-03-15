25cedd88d5198f2672f0807a6f35886785294055
Johannes HEINZL <jh.heinzl@gmail.com>
2023-03-15 21:20:55 +0100

    Updates.

--- vagrant/package.sh
+++ vagrant/package.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash -e
 vagrant package --base inspectit-ocelot
 vagrant box add jhheinzl/inspectit-ocelot package.json
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 vagrant up
 vagrant snapshot push
 echo "Setup Finished"
