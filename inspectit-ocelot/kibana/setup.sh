9d48ab66d5b84c786daaa4bc7b93fb9146093153
Johannes HEINZL <jh.heinzl@gmail.com>
2023-06-10 20:14:39 +0200

    Updates.

--- inspectit-ocelot/kibana/setup.sh
+++ inspectit-ocelot/kibana/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
-# Unlock Elasticsearch on low disk space
+# Decrease elasticsearch low disk space lock threshold
+curl -k -u "admin:demo" -X PUT https://localhost:9200/_cluster/settings \
+-H "Content-Type: application/json" -d '{"transient": { "cluster.routing.allocation.disk.watermark.flood_stage": "99%"}}'
+
+# Unlock elasticsearch on low disk space
 curl -k -u "admin:demo" -X PUT https://localhost:9200/_all/_settings \
 -H "Content-Type: application/json" -d '{"index.blocks.read_only_allow_delete": null}'
 
